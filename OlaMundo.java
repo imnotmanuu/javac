5c13631f0af78c1c475f91cf2491b22f321623fc
emanuelly <159542297+imnotmanuu@users.noreply.github.com>
2025-08-08 08:35:06 -0300

    Add files via upload

--- OlaMundo.java
+++ OlaMundo.java
@@ -2,18 +2,6 @@ public class OlaMundo {
 
     public static void main(String[] args) {
         System.out.println("Ola Mundo");
-        //tipo nome = conteudo;
 
-        String meuNome = "manu ";
-        meuNome += "Schneider";
-        int idade = 17;
-        double altura = 1.67;
-        char sexo = 'F';
-        boolean casado = true;
-
-        
-        System.out.println("Meu nome é "+meuNome+);
-        System.out.println("Minha idade é "+minhaIdade);
-        System.out.println("Minha altura é "+minhaIdade+);
             }
 }
---
+++ variaveis.java
@@ -0,0 +1,20 @@
+public class variaveis {
+           public static void main(String[] args) {
+
+
+        String meuNome = "manu ";
+        meuNome += "Schneider";
+        int idade = 17;
+        double altura = 1.67;
+        char sexo = 'F';
+        boolean casado = false;
+
+        
+        System.out.println(" Meu nome é " + meuNome);
+        System.out.println(" minha idade é " + idade + " anos");
+        System.out.println(" Minha altura " + altura);
+        System.out.println(" sou casado? " + casado);
+        System.out.println(" Sou do sexo " + sexo);
+
+}
+}
